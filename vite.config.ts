11c5961fb334eeb6b315e1fe1edf054fb8ad2123
monman53 <tetsuro53@gmail.com>
2024-07-27 07:16:39 +0900

    Setup github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: 'https://monman53.github.io/fractal-chamber/'
 })
