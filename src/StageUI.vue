3cf469229d858a8160f961dedd42d2c90079ba9a
monman53 <tetsuro53@gmail.com>
2024-07-27 07:14:04 +0900

    Fold menu

--- src/StageUI.vue
+++ src/StageUI.vue
@@ -18,25 +18,28 @@ const fractalMode = () => {
 
 <template>
   <div id="base">
-    <!-- {{ app.width }}, {{ app.height }} -->
-    <!-- <br> -->
-    FPS: {{ humanReadable(fps) }}<br />
-    <template v-for="(prop, key) in parameterProps" :key="key">
-      <template v-if="prop.type === 'range'">
-        <label>
-          {{ key }}: {{ humanReadable(parameter[key]) }}<br />
-          <input
-            type="range"
-            v-model.number="parameter[key]"
-            :step="prop.step"
-            :min="prop.min"
-            :max="prop.max"
-          />
-        </label>
-        <br />
+    <details>
+      <summary>parameters</summary>
+      <!-- {{ app.width }}, {{ app.height }} -->
+      <!-- <br> -->
+      <!-- FPS: {{ humanReadable(fps) }}<br /> -->
+      <template v-for="(prop, key) in parameterProps" :key="key">
+        <template v-if="prop.type === 'range'">
+          <label>
+            {{ key }}: {{ humanReadable(parameter[key]) }}<br />
+            <input
+              type="range"
+              v-model.number="parameter[key]"
+              :step="prop.step"
+              :min="prop.min"
+              :max="prop.max"
+            />
+          </label>
+          <br />
+        </template>
       </template>
-    </template>
-    <button @click="fractalMode">fractal mode</button>
+      <button @click="fractalMode">fractal mode</button>
+    </details>
   </div>
 </template>
 
